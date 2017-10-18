572424427fee09910b521cce7e5cbb9c666d0cb1
Tamas_Sasfalvi <tamas_sasfalvi@epam.com>
2017-10-18 12:32:12 +0200

    rename

--- src/main/java/com/Application.java
+++ src/main/java/com/Application.java
@@ -3,7 +3,7 @@ package com;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(scanBasePackages = "com.epam.*")
+@SpringBootApplication(scanBasePackages = "com.*")
 public class Application {
 
     public static void main(String[] args) {
